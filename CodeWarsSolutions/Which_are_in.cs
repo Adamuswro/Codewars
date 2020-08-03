136c5f3031fdfab6dff71cb2fcedbc27186e15a4
Adam Szulc <adszulc@danfoss.com>
2020-08-03 13:39:17 +0200

    Solution

---
+++ CodeWarsSolutions/Which_are_in.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsSolutions
+{
+    class Which_are_in
+    {
+        public static string[] inArray(string[] array1, string[] array2)
+        {
+            return array1.Where(s => array2.Any(p => p.Contains(s))).OrderBy(v=>v).ToArray();
+        }
+    }
+}
